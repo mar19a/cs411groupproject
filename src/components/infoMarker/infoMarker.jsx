cc8a6ac7c10177fbb72e415072e3e0592f4eae16
mar19a <mariano@bu.edu>
2023-03-13 00:30:53 -0400

    Syntax error

--- src/components/infoMarker/infoMarker.jsx
+++ src/components/infoMarker/infoMarker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 
-import "./InfoMarker.css";
+import "./infoMarker.css";
 
 const InfoMarker = ({ info, handleClick }) => {
   return (
