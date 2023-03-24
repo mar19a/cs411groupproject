604615f914760ff5f6923c92bc9e367cd078295f
mar19a <mariano@bu.edu>
2023-03-24 02:50:18 -0400

    News completed

--- src/components/NewsPage/NewsItem.js
+++ src/components/NewsPage/NewsItem.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import './NewsItem.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NewsItem.css';
 
 const NewsItem = ({ title, description, url, urlToImage }) => {
-    return (
-        <div className="news-app">
-            <div className='news-item'>
-                <img className='news-img' src={urlToImage} alt={urlToImage} />
-                <h3><a href={url}>{title}</a></h3>
-                <p>{description}</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="news-app">
+      <div className='news-item'>
+        <img className='news-img' src={urlToImage} alt={title} />
+        <h3 className='news-title'><a href={url} rel="noopener noreferrer" target="_blank">{title}</a></h3>
+        <p className='news-description'>{description}</p>
+      </div>
+    </div>
+  );
+};
 
-export default NewsItem
+NewsItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  urlToImage: PropTypes.string.isRequired,
+};
+
+export default NewsItem;
--- src/components/NewsPage/NewsPage.js
+++ src/components/NewsPage/NewsPage.js
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import NewsItem from './NewsItem'
-import './NewsPage.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NewsItem from './NewsItem';
 
 const NewsPage = () => {
+  const [articles, setArticles] = useState([]);
 
-  const [articles, setArticles] = useState([])
   useEffect(() => {
     const getArticles = async () => {
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=massachusetts&from=2023-02-24&sortBy=publishedAt&apiKey=1cbb1ed4bef44f50ad4016f01131e3e4`)
-        setArticles(response.data.articles)
-        console.log(response)
-    }
-    getArticles()
-  }, [])
+      try {
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=massachusetts&from=2023-02-24&sortBy=publishedAt&apiKey=1cbb1ed4bef44f50ad4016f01131e3e4`);
+        setArticles(response.data.articles);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getArticles();
+  }, []);
 
   return (
-    <div className="news-grid">
-      {articles.map((article, index) => {
-        return (
+    <div className='news-page'>
+      <h1 className='news-page-title'>Latest News from Massachusetts</h1>
+      <div className='news-list'>
+        {articles.map((article, index) => (
           <NewsItem
             key={index}
             title={article.title}
@@ -26,10 +31,10 @@ const NewsPage = () => {
             url={article.url}
             urlToImage={article.urlToImage}
           />
-        )
-      })}
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default NewsPage;
