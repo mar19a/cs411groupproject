71fa1faf1704c9b4dde2a729994b2d072c444002
mar19a <mariano@bu.edu>
2023-04-09 20:24:31 -0400

    Login is ready

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -3,35 +3,51 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "signup",
-});
+const con = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "register"
+})
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to the MySQL database!");
-});
+app.post('/signup', (req, res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.post("/signup", (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  const query = `INSERT INTO users (first_name, last_name, email, password) VALUES ('${firstName}', '${lastName}', '${email}', '${password}')`;
-  
-  db.query(query, (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "An error occurred while inserting the user into the database" });
-    } else {
-      res.status(201).json({ message: "User created successfully", data: req.body });
-    }
-  });
-});
+    con.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, password], 
+        (err, result) => {
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: "ENTER CORRECT ASKED DETAILS!"})
+            }
+        }
+    )
+})
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
-  });
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    con.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], 
+        (err, result) => {
+            if(err){
+                req.setEncoding({err: err});
+            }else{
+                if(result.length > 0){
+                    res.send(result);
+                }else{
+                    res.send({message: "WRONG USERNAME OR PASSWORD!"})
+                }
+            }
+        }
+    )
+})
+
+app.listen(3051, () => {
+    console.log("running backend server");
+})
